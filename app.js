47f65936e109d5e4a8911fdca49e0d6305dc0f02
vasya <vasya.com>
2023-09-30 16:28:47 +0500

    js6

---
+++ app.js
@@ -0,0 +1,16 @@
+let user =  [];
+
+for (let i = 0; i < Infinity; i++) {
+let text = prompt('Введите действие и имя через запятую и пробел')
+ if (text == 'stop') { break}
+ 
+let array = text.split(', ')
+array[0] == 'add' ? user.push(array[1]) : ' '  ;
+
+if (array[0] == 'del') {
+    for(let key in user) {
+    user[key] == array[1] ? user.splice(key, 1) : ''}
+};
+console.log(user);
+}
+console.log(user);
